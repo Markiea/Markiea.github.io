88204c50705c7c4ce6d2c9144998a8ef02977e46
Markie Chancellor <43099384+Markiea@users.noreply.github.com>
2018-11-08 14:41:52 -0500

    Update myJavaScript2.js

--- myJavaScript2.js
+++ myJavaScript2.js
@@ -1,3 +1,5 @@
 // Prompt
 var answer = prompt ('Who is the most famous medieval female figure in European history?');
-if( answer == 'Joan of Arc' || 'Joan') {document.write("That is Right!");}
+if(answer === 'Joan of Arc' || answer === 'Joan') {document.write("That is Right!");}
+else{ prompt ('Her name starts with J and she has Arc in it'); }
+
